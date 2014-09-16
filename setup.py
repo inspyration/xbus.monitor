68ba881147b4e700bb685594617e6eac3ede245f
Florent Aide <florent.aide@gmail.com>
2014-09-16 16:27:49 +0200

    use namespaces for xbus.monitor

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
@@ -25,15 +24,15 @@ setup(name='xbus.monitor',
       description='XBus Monitor',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
+          "Programming Language :: Python",
+          "Framework :: Pyramid",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+      ],
       author='',
       author_email='',
       url='',
-      keywords='web wsgi bfg pylons pyramid',
+      keywords='xbus web pyramid',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
