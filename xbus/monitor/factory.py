f48a3b7164ecb0cf3141e9cb4122ab9af33fcf27
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2014-09-23 12:32:19 +0200

    Protect against invalid IDs

--- xbus/monitor/factory.py
+++ xbus/monitor/factory.py
@@ -1,9 +1,18 @@
+from pyramid.httpexceptions import HTTPBadRequest
+
 from xbus.monitor.models.models import DBSession
 from xbus.monitor.models.models import EventType
 
 
-def eventtype_factory(request):
-    eventtype_id = request.matchdict.get('id')
+def _get_record_id(request):
+    try:
+        return int(request.matchdict.get('id'))
+    except:
+        raise HTTPBadRequest(json_body={"error": "Invalid ID"})
+
+
+def event_type(request):
+    record_id = _get_record_id(request)
     query = DBSession.query(EventType)
-    query = query.filter(EventType.id == eventtype_id)
+    query = query.filter(EventType.id == record_id)
     return query.first()
