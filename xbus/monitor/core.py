74425dbae9617c4abcb0eb003ba527ef548a2b38
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2015-01-09 15:33:28 +0100

    API to replay envelopes

--- xbus/monitor/core.py
+++ xbus/monitor/core.py
@@ -151,6 +151,7 @@ def main(global_config, **settings):
 
     # Other parts of the API.
 
+    config.add_route('replay_envelope', API_PREFIX + 'replay_envelope')
     config.add_route('upload', API_PREFIX + 'upload')
     config.add_route('xml_config', API_PREFIX + 'xml_config')
 
