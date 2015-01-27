79135695ca67c4ae6315eb55eb84580464cb4a40
Houzefa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2015-01-27 12:35:51 +0100

    Improve the consumer list

--- xbus/monitor/consumers.py
+++ xbus/monitor/consumers.py
@@ -13,6 +13,9 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 
+from xbus.monitor.models.monitor import DBSession
+from xbus.monitor.models.monitor import Role
+
 
 log = logging.getLogger(__name__)
 
@@ -28,17 +31,10 @@ def _make_session(db_url):
 # Store consumers and data clearing database URLs and 2 different arrays to
 # make sure no database information is inadvertently leaked.
 
-_consumers = [
-    {'id': 'abcd', 'name': 'consumer with clearing', 'clearing': True},
-    {'id': 'efgh', 'name': 'consumer without clearing'},
-]
+_consumers = []
 
 # {consumer ID: SQLAlchemy session object to the data clearing database}
-_consumer_clearing_sessions = {
-    'abcd': _make_session(
-        'postgresql://xbus:xbus@localhost:5432/xbus_clearinghouse'
-    ),
-}
+_consumer_clearing_sessions = {}
 
 
 def get_consumers():
@@ -79,10 +75,10 @@ def _request_consumers(front_url, login, password, loop):
     client = yield from rpc.connect_rpc(connect=front_url, loop=loop)
     log.debug('RPC connection OK')
     token = yield from client.call.login(login, password)
-    log.debug('Got connection token: %s' % token)
+    log.debug('Got connection token: %s', token)
 
     consumers = yield from client.call.get_consumers(token)
-    log.debug('Request to refresh consumers sent')
+    log.debug('Got the consumer list: %s', consumers)
 
     yield from client.call.logout(token)
     log.debug('Logged out; terminating')
@@ -97,6 +93,10 @@ def refresh_consumers():
     """Ask Xbus for a fresh new list of Xbus consumers.
     """
 
+    # Global arrays we are going to refresh.
+    global _consumers
+    global _consumer_clearing_sessions
+
     # TODO config params
     # The login & password must exist in the "emitter" database table.
     front_url = 'tcp://127.0.0.1:1984'
@@ -106,4 +106,29 @@ def refresh_consumers():
     # Send our request via 0mq to the Xbus front-end.
     zmq_loop = aiozmq.ZmqEventLoopPolicy().new_event_loop()
     emitter = _request_consumers(front_url, login, password, zmq_loop)
-    _consumers = zmq_loop.run_until_complete(emitter)
+    role_ids = zmq_loop.run_until_complete(emitter)
+
+    # TODO Use the db to get info about roles? or just save role IDs?
+    # TODO How to fetch data clearing information?
+
+    session = DBSession()
+    roles = session.query(Role).filter(Role.id.in_(role_ids))
+    _consumers = [
+        {
+            'clearing': True,  # TODO Implement.
+            'id': 'consumer-%d' % i,  # TODO Implement.
+            'name': role.service.name,
+        }
+        for i, role in enumerate(roles)
+    ]
+
+    # Refresh the list of consumers with data clearing cache.
+    # TODO Implement.
+    data_clearing_db_url = (
+        'postgresql://xbus:xbus@localhost:5432/xbus_clearinghouse'
+    )
+    _consumer_clearing_sessions = {
+        'consumer-%d' % i: _make_session(data_clearing_db_url)
+        for i, role in enumerate(roles)
+        if data_clearing_db_url
+    }
